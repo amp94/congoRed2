6cf8b913b7152f7d615d4516b1e5be0458e9c426
amp94 <angelicamp94@gmail.com>
2016-05-08 16:43:37 +0000

    updated howto page to address user complaints

--- models/Results.js
+++ models/Results.js
@@ -11,10 +11,10 @@ var Results = new keystone.List('Results',{ sortable: true, defaultSort: 'img_na
 
 
 Results.add({
-  img_name:  { type: String, required: true },
-  img_url: { type: Types.Url, required: true },
-  result:   { type: String, required: true },
-  publishedDate: { type: Date, default: Date.now },
+  img_name:  { type: String, required: true, initial: false, noedit: true },
+  img_url: { type: Types.Url, required: true, initial: false, noedit: true },
+  result:   { type: String, required: true, initial: false, noedit: true },
+  publishedDate: { type: Date, default: Date.now, noedit: true },
 });
 
 // Provide access to Keystone
